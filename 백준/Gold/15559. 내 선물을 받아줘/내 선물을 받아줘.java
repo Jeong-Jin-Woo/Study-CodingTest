03ed30e337b6b7be4d93cb7367a85ccd0eadcfbb
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2023-10-30 16:37:59 +0900

    [Gold II] Title: 내 선물을 받아줘, Time: 676 ms, Memory: 30600 KB -BaekjoonHub

--- 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.java
+++ 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.java
@@ -3,41 +3,23 @@ public class Main{
 	static int n,m;
 	static char[][] map;
 	static int[][] visit;
+	static char[] direction = {'W','E','N','S'};
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {-1,1,0,0};
 	static boolean dfs(int x, int y, int count) {
 		if(visit[x][y] == count) return true;
-			if(map[x][y] == 'W' && visit[x][y] <= count) {
+		for(int i=0; i<4; i++) {
+			if(map[x][y] == direction[i] && visit[x][y] <= count) {
 				visit[x][y] += count;
-				if(dfs(x, y-1, count)) return true;
+				int nx = x + dx[i];
+				int ny = y + dy[i];
+				if(dfs(nx, ny, count)) return true;
 				else {
 					visit[x][y] -= count;
 					return false;
 				}
 			}
-			else if(map[x][y] == 'E'&& visit[x][y] <= count) {
-				visit[x][y] += count;
-				if(dfs(x, y+1, count)) return true;
-				else {
-					visit[x][y] -= count;
-					return false;
-				}
-			}
-			else if(map[x][y] == 'N'&& visit[x][y] <= count) {
-				visit[x][y] += count;
-				if(dfs(x-1, y, count))return true;
-				else {
-					visit[x][y] -= count;
-					return false;
-				}
-			}
-			else if(map[x][y] == 'S'&& visit[x][y] <= count) {
-				visit[x][y] += count;
-				if(dfs(x+1, y, count))return true;
-				else {
-					visit[x][y] -= count;
-					return false;
-				}
-			}
-		
+		}
 		return false;
 	}
 	public static void main(String args[]) {
