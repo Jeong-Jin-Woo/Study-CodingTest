2523b96ad6035663f226574e32612abf1862827a
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2023-11-13 23:27:28 +0900

    [Silver IV] Title: 카드 놓기, Time: 260 ms, Memory: 19008 KB -BaekjoonHub

--- 백준/Silver/5568. 카드 놓기/카드 놓기.java
+++ 백준/Silver/5568. 카드 놓기/카드 놓기.java
@@ -2,25 +2,17 @@ import java.util.*;
 public class Main{
 	static int n,k;
 	static int[] arr;
-	static int[] set;
 	static boolean[] visit;
-	static ArrayList<String> has = new ArrayList<>();
-	static void dfs(int count, int size) {
-		if(count == size) {
-			String str = "";
-			for(int i=0; i<size; i++) {
-				str += String.valueOf(set[i]);
-			}
-			if(!has.contains(str))
-				has.add(str);
+	static HashSet<String> has = new HashSet<>();
+	static void dfs(int count, String str) {
+		if(count == k) {
+			has.add(str);
 			return;
 		}
 		for(int i=0; i<n; i++) {
 			if(!visit[i]){
 				visit[i] = true;
-				set[count] = arr[i];
-				dfs(count+1, size);
-				set[count] = 0;
+				dfs(count+1, arr[i] + str);
 				visit[i] = false;
 			}
 		}
@@ -30,12 +22,11 @@ public class Main{
 		n = in.nextInt();
 		k = in.nextInt();
 		arr = new int[n];
-		set = new int[n];
 		visit = new boolean[n];
 		for(int i=0; i<n; i++) {
 			arr[i] = in.nextInt();
 		}
-		dfs(0, k);
+		dfs(0, "");
 		System.out.println(has.size());
 	}
 }
