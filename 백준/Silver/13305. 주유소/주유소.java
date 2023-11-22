95d5558f4d9d52b98241217027aa3fd2f9da9f63
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2023-11-22 17:01:53 +0900

    [Silver III] Title: 주유소, Time: 1168 ms, Memory: 183252 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,25 +5,26 @@ public class Main{
 		int n = in.nextInt();
 		int[] km = new int[n-1];
 		int[] node = new int[n];
-		int sum = 0;
 		for(int i=0; i<n-1; i++) {
 			km[i] = in.nextInt();
-			sum += km[i];
 		}
 		int min = (int)1e9;
 		for(int i=0; i<n; i++) {
 			node[i] = in.nextInt();
-			if(i != n-1)
-				min = Math.min(min, node[i]);
 		}
-		int answer = 0;
+		long answer = 0;
 		for(int i=0; i<n-1; i++) {
-			if(min == node[i]) {
-				answer += node[i] * sum;
-				break;
+			int count = 0;
+			long sum = 0;
+			for(int j=i; j<n-1; j++) {
+				if(node[i] > node[j]) {
+					break;
+				}
+				count += 1;
+				sum += km[j];
 			}
-			answer += node[i] * km[i];
-			sum -= km[i];
+			answer += node[i] * sum;
+			i += count-1;
 		}
 		System.out.println(answer);
 	}
