1a94fbe6a818fd55715df502de33cbee5bc874c6
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2024-03-29 23:52:53 +0900

    [Silver II] Title: 최소 힙, Time: 1064 ms, Memory: 113196 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,34 @@
+import java.util.*;
+class Position implements Comparable<Position>{
+	public int x;
+	Position(int x){
+		this.x=x;
+	}
+	@Override
+	public int compareTo(Position other) {
+		return Integer.compare(x, other.x);
+	}
+}
+public class Main{
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		PriorityQueue<Position> pq = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<n; i++) {
+			int x = in.nextInt();
+			if(x != 0) {
+				pq.offer(new Position(x));
+			}
+			else {
+				if(pq.isEmpty()) {
+					sb.append(0 + "\n");
+				}
+				else {
+					sb.append(pq.poll().x + "\n");
+				}
+			}
+		}
+		System.out.println(sb);
+	}
+}
