eb9b8dec55528f1e454e1a755b3e7ed4a4c46620
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2024-05-07 23:19:16 +0900

    [level 2] Title: 최솟값 만들기, Time: 5.70 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution{
+    public int solution(int []A, int []B){
+        int answer = 0;
+        int n = A.length;
+        PriorityQueue<Integer> Apq = new PriorityQueue<>();
+        PriorityQueue<Integer> Bpq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i=0; i<n; i++){
+            Apq.offer(A[i]);
+            Bpq.offer(B[i]);
+        }
+        while(!Apq.isEmpty()){
+            answer += (Apq.remove() * Bpq.remove());
+        }
+        return answer;
+    }
+}
