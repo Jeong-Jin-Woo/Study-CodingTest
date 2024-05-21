a29cec556f64ab23382a3b9874638077f008f042
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2024-05-21 16:52:31 +0900

    [level 2] Title: NULL 처리하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
+++ 프로그래머스/2/59410. NULL 처리하기/NULL 처리하기.sql
@@ -2,5 +2,5 @@
 # SELECT animal_type, NULLIF(name,"No name"), sex_upon_intake
 # from animal_ins;
 
-SELECT animal_type, IFNULL(NAME,"No name") name, sex_upon_intake
+SELECT animal_type, IFNULL(NAME, "No name") name, sex_upon_intake
 from animal_ins;
