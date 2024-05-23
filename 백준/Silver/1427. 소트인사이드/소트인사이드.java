7a3d946cea170d00486d76c4080d3924984eb39c
Jeong-Jin-Woo <98380114+Jeong-Jin-Woo@users.noreply.github.com>
2024-05-23 23:54:22 +0900

    [Silver V] Title: 소트인사이드, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String N = br.readLine();
+        Integer[] array = new Integer[N.length()];
+
+        for(int i = 0; i < array.length; i++) {
+            array[i] = N.charAt(i) - '0';
+        }
+
+        Arrays.sort(array, Collections.reverseOrder());
+
+        for(int i = 0; i < array.length; i++) {
+            bw.write(array[i]+"");
+        }
+
+        bw.flush();
+        bw.close();
+
+    }
+}
